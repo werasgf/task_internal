c3763011a48dd4f5e6cd5cd97b0edb70e1dfa40d
werasgf <tasher02@list.ru>
2023-02-14 12:41:29 +0300

    33

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
-package task.app;
-
 import task.console.Menu;
 public class App {
     public static void main(String[] args) {
+
+
+
+        /*
         Menu menu = new Menu();
         menu.mainMenu();
+        */
     }
 }
